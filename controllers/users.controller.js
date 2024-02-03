4ec3248fa7935b087bbf0b02d1d3e41827eaeb01
satyam shrivastava <satyamshrivastava683@gmail.com>
2024-02-04 02:41:28 +0530

    completed view section expcept Test section

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,5 +2,11 @@ class UserController{
     aboutView(req,res){
         res.render("about",{title:"AboutUs"});
     }
+    signupView(req,res){
+        res.render("signup",{title:"Sign|Up"});
+    }
+    signinView(req,res){
+        res.render("signin",{title:"Sign|In"});
+    }
 }
 export default UserController;
--- routers/user.js
+++ routers/user.js
@@ -3,4 +3,6 @@ import UserController from "../controllers/users.controller.js";
 const route=Router();
 const userController=new UserController();
 route.get("/about",userController.aboutView);
+route.get("/signup",userController.signupView);
+route.get("/signin",userController.signinView);
 export default route
